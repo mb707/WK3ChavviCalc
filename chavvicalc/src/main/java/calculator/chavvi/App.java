fbf89914b12858d4cf6162f7624562e92a24e637
mb707 <112012511+mb707@users.noreply.github.com>
2024-09-12 21:04:44 -0700

    string, print lines, while loop
    started making my string, print lines, and building my while loop for the greetings and inputs.

--- chavvicalc/src/main/java/calculator/chavvi/App.java
+++ chavvicalc/src/main/java/calculator/chavvi/App.java
@@ -1,13 +1,24 @@
 package calculator.chavvi;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
- *
+ * Chavvi Calculator
+ * By Morgan Beebe CSIS27 Fall 2024
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Friends!" );
+        Scanner scanner = new Scanner(System.in);
+        float a = 0;
+        float b = 0;
+        String input;
+        System.out.println("Welcome to Chavvi's Calculator");
+        System.out.println("Please only use one of these commands a, b, +, -, *, /, c, q");
+        while (true) { 
+            
+        }
     }
 }
