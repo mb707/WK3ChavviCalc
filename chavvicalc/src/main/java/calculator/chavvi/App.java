05738e5a8e39729b70094bcdaa191f15a94ff6b7
mb707 <112012511+mb707@users.noreply.github.com>
2024-09-12 21:16:18 -0700

    yoda, added input
    added Master Yoda for moral support and input to make sure it works.

--- chavvicalc/src/main/java/calculator/chavvi/App.java
+++ chavvicalc/src/main/java/calculator/chavvi/App.java
@@ -3,22 +3,38 @@ package calculator.chavvi;
 import java.util.Scanner;
 
 /**
- * Chavvi Calculator
- * By Morgan Beebe CSIS27 Fall 2024
- */
+* Chavvi Calculator
+* By Morgan Beebe CSIS27 Fall 2024
+* """
+*                          .--.                  Try not.
+*               ::\\`--._,'.::.`._.--'/::     Do or do not.
+*                ::::.  ` __::__ '  .::::    There is no try.
+*                ::::::-:.`'..`'.:-::::::
+*               ::::::::\\ `--' /::::::::              -Yoda"""
+*/
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello Friends!" );
+
         Scanner scanner = new Scanner(System.in);
+
         float a = 0;
         float b = 0;
         String input;
+
         System.out.println("Welcome to Chavvi's Calculator");
         System.out.println("Please only use one of these commands a, b, +, -, *, /, c, q");
+
         while (true) { 
-            
+            System.out.println("Enter a command: ");
+            input = scanner.next();
+
+            if (input.equals("a")){
+                System.out.println("Please enter a number for A: ");
+                
+            }
         }
     }
 }
